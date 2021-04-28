420129da40b23ed1c5f13f704376a67180e6d700
Bhaktij Koli <bhaktijkoli121@gmail.com>
2021-04-28 11:29:03 +0530

    Added prisma zoom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import ImageGallery from 'react-image-gallery';
-import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import PrismaZoom from 'react-prismazoom';
 import './App.css';
 import "react-image-gallery/styles/css/image-gallery.css";
-import { Fragment, useState } from 'react';
+import { Fragment, useRef, useState } from 'react';
 
 
 const images = [
@@ -52,35 +52,24 @@ const App = () => {
 }
 
 const Item = ({ item }) => {
-  console.log(item);
   const [page, setPage] = useState(0)
+  const prismaRef = useRef()
   return (
     <Fragment>
       <div className="item">
-        <TransformWrapper
-          defaultScale={1}
-          defaultPositionX={200}
-          defaultPositionY={100}
-          options={{
-            limitToBounds: true
-          }
-          }
-        >
-          {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
-            <Fragment>
-              <div className="page-buttons">Page {page + 1}/{item.pages.length}
-                <button disabled={page === item.pages.length} onClick={e => setPage(page + 1)}>Next</button>
-                <button disabled={page === 0} onClick={e => setPage(page - 1)}>Prev</button>
-                <button onClick={zoomIn}>+</button>
-                <button onClick={zoomOut}>-</button>
-                <button onClick={resetTransform}>x</button>
-              </div>
-              <TransformComponent>
-                <img src={item.pages[page].original} className="image-gallery-image" />
-              </TransformComponent>
-            </Fragment>
-          )}
-        </TransformWrapper>
+        <div className="page-buttons">
+          Page {page + 1}/{item.pages.length}
+          <button disabled={page === item.pages.length} onClick={e => setPage(page + 1)}>Next</button>
+          <button disabled={page === 0} onClick={e => setPage(page - 1)}>Prev</button>
+          <button disabled={page === 0} onClick={e => setPage(page - 1)}>+</button>
+        </div>
+        <div className="item-image-container">
+          <div className="item-image">
+            <PrismaZoom ref={prismaRef} scrollVelocity={0.5}>
+              <img src={item.pages[page].original} />
+            </PrismaZoom>
+          </div>
+        </div>
       </div>
     </Fragment >
   )
